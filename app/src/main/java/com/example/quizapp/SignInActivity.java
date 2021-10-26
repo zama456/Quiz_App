e7eb0fbb2b519d90e3fcb09002dc60f6d619ddc8
zama456 <77655925+zama456@users.noreply.github.com>
2021-10-26 22:22:38 +0530

    new commit

--- app/src/main/java/com/example/quizapp/SignInActivity.java
+++ app/src/main/java/com/example/quizapp/SignInActivity.java
@@ -3,8 +3,10 @@ package com.example.quizapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -22,12 +24,13 @@ import com.google.firebase.auth.FirebaseUser;
 import java.text.BreakIterator;
 
 public class SignInActivity extends AppCompatActivity {
-    
+
     TextView tvSignup;
-    EditText etEmail,etPassword;
+    EditText etEmail, etPassword;
     Button btnSignIn;
     FirebaseAuth mAuth;
     FirebaseUser mUser;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +38,9 @@ public class SignInActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_in);
 
         tvSignup = findViewById(R.id.tvSignup);
-        etEmail = findViewById(R.id.etEmail);
-        etPassword = findViewById(R.id.etPassword);
+        etEmail = findViewById(R.id.etEmailSignIn);
+        etPassword = findViewById(R.id.etPasswordSignIn);
+        progressDialog = new ProgressDialog(this);
         btnSignIn = findViewById(R.id.btnSignIn);
         mAuth = FirebaseAuth.getInstance();
         mUser = mAuth.getCurrentUser();
@@ -53,12 +57,48 @@ public class SignInActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-
+                perforSignIn();
 
             }
 
         });
-
     }
 
+    private void perforSignIn() {
+
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+
+        if (email.isEmpty()) {
+            etEmail.setError("please enter your Email!");
+            etEmail.requestFocus();
+        } else if (password.isEmpty() || password.length() < 6) {
+            etPassword.setError("please enter correct password");
+            etPassword.setError("password must not be <6 !");
+            etPassword.requestFocus();
+        } else {
+            progressDialog.setMessage("please wait while Signing In....");
+            progressDialog.setTitle("SignIn");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                    if (!task.isSuccessful()) {
+                        progressDialog.dismiss();
+                        Toast.makeText(SignInActivity.this, "Error SignIn Failed !", Toast.LENGTH_SHORT).show();
+                    } else {
+                        if (task.isSuccessful())
+                            Toast.makeText(SignInActivity.this, "SignIn Successful" + task.getException(), Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(SignInActivity.this, MainActivity.class));
+                        finish();
+                    }
+                }
+
+            });
+
+        }
+    }
 }
--- app/src/main/java/com/example/quizapp/SignUpActivity.java
+++ app/src/main/java/com/example/quizapp/SignUpActivity.java
@@ -3,7 +3,9 @@ package com.example.quizapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -22,15 +24,16 @@ import com.google.firebase.auth.FirebaseUser;
 public class SignUpActivity<SingUpActivity> extends AppCompatActivity {
 
     TextView tvAlreadyaccount;
-    EditText etUsername,etEmail,etPassword;
+    EditText etUsername, etEmail, etPassword;
     Button btnSignup;
+    AlertDialog alertDialog;
     FirebaseAuth mAuth;
     FirebaseUser mUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
 
         tvAlreadyaccount = findViewById(R.id.tvAlreadyaccount);
@@ -41,6 +44,30 @@ public class SignUpActivity<SingUpActivity> extends AppCompatActivity {
         etUsername = findViewById(R.id.etUsername);
         mAuth = FirebaseAuth.getInstance();
         mUser = mAuth.getCurrentUser();
+
+
+           {
+            AlertDialog.Builder alretdialog = new AlertDialog.Builder(this);
+            alretdialog.setTitle("Warning!!");
+            alretdialog.setIcon(R.drawable.ic_baseline_warning_24);
+            alretdialog.setCancelable(false);
+            alretdialog.setMessage("Are sure you want to exit from this app??");
+            alretdialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+            alretdialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+
+                }
+            });
+            AlertDialog alert = alretdialog.create();
+            alretdialog.show();
+        }
 
         tvAlreadyaccount.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,31 +91,33 @@ public class SignUpActivity<SingUpActivity> extends AppCompatActivity {
                 String username = etUsername.getText().toString();
                 String password = etPassword.getText().toString();
 
+                if (username.isEmpty()) {
+                    etUsername.setError("Username must not be empty!");
+                }
                 if (email.isEmpty()) {
-                    etEmail.setError("please enter your Email");
+                    etEmail.setError("please enter your Email!");
                     etEmail.requestFocus();
                 } else if (password.isEmpty() || password.length() < 6) {
-                    etPassword.setError("please enter your password");
+                    etPassword.setError("password must not <6!");
                     etPassword.requestFocus();
-                } else if (!email.isEmpty() && !password.isEmpty())
-                {
+                } else if (!email.isEmpty() && !password.isEmpty()) {
                     progressDialog.setMessage("please wait while signing up....");
                     progressDialog.setTitle("Signup");
                     progressDialog.setCanceledOnTouchOutside(false);
                     progressDialog.show();
-                    Toast.makeText(SignUpActivity.this, "This email is already exist", Toast.LENGTH_SHORT).show();
+
 
                     mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (!task.isSuccessful()) {
                                 progressDialog.dismiss();
-                                Toast.makeText(SignUpActivity.this, "SignUp Failed !", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(SignUpActivity.this, "Error SignUp Failed !", Toast.LENGTH_SHORT).show();
                             } else {
-                                if(task.isSuccessful())
+                                if (task.isSuccessful())
                                     Toast.makeText(SignUpActivity.this, "SignUp Successful", Toast.LENGTH_SHORT).show();
                                 startActivity(new Intent(SignUpActivity.this, SignInActivity.class));
-                               //Intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                                finish();
                             }
                         }
 
@@ -97,5 +126,10 @@ public class SignUpActivity<SingUpActivity> extends AppCompatActivity {
                 }
             }
         });
+
+
     }
-}
+
+    }
+
+
